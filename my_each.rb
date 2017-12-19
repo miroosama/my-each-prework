e276a6532a2730e3d48e281a1871195b63335426
osama miro <o.miro650@gmail.com>
2017-12-19 21:09:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,13 @@
 def my_each(array) # put argument(s) here
   # code here
   if block_given?
-
+my_array = []
   i = 0
   while i < array.length
-  array[i]
+  my_array.push(array[i])
     i = i + 1
   end
-  array
+  my_array
 else
  "nothing"
 
