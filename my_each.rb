12b01282ea8e3ae6be619cc328bf64efb1be8c4e
osama miro <o.miro650@gmail.com>
2017-12-19 21:14:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
   i = 0
-  if block_given?
-  while i < array.size
-yeild(array[i])
-    i += 1
-  end
-else
- "nothing"
-
-end
-array
-end
+   while i < array.size
+     block_given? # optional
+     yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+     i += 1
+   end
+ array
+ end
